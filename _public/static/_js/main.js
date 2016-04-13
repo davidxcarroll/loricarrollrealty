d34f60183c10780ad5b1f8c31c9718e89c26722f
David <kansas@inkwhiskey.com>
2016-04-13 10:04:59 -0400

    some responsive tweaks

--- _public/static/_js/main.js
+++ _public/static/_js/main.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
     slidesToScroll: 3,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
       },
       {
-        breakpoint: 410,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
